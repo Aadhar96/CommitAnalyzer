e2e466e774e25c65c1c8839a3f9a8bb7db852471
Aadhar96 <sachdevaaadhar@gmail.com>
2017-07-13 17:34:09 +0530

    Update and rename Dataset.py to Link_extractor.py

--- Link_extractor.py
+++ Link_extractor.py
@@ -13,20 +13,22 @@ def find(a):
                 ans.append(i)
                 return ans
 
-MAX=179
 a=webdriver.Chrome('/Users/aadharsachdeva/Downloads/chromedriver')
-link='https://github.com/jquery/jquery/commits/master?after=3fcddd6e72e7e318c0b062e391d60867732318ae+4024'
+link='https://github.com/jquery/jquery/commits/master'
 a.get(link)
 index=find(link)
 file_name='commlinks_'+link[index[0]+1:index[1]]
 data=open(file_name,'wb')
 links=[]
-for _ in range(MAX):
+while(1):
     b=a.find_elements_by_class_name('commit-title')
     for i in b:
         tmp=i.find_element_by_tag_name('a')
         links.append(tmp.get_property('href'))
-    tmp=a.find_elements_by_partial_link_text("Older")
+    try:
+        tmp=a.find_elements_by_partial_link_text("Older")
+    except:
+        break
     tmp=tmp[-1]
     try:
         a.get(tmp.get_property('href'))
